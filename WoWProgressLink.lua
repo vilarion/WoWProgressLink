97ce1302236e8f292fc7948e93413cab3c7738f4
Vilarion <vilarion@illarion.org>
2017-07-21 19:03:54 +0200

    Add support for Raider.IO
    * You can now press ALT while double clicking to get a Raider.IO link in the LFG tool
    * You can also set a key binding for links via mouse over player frames

--- WoWProgressLink.lua
+++ WoWProgressLink.lua
@@ -1,5 +1,6 @@
 BINDING_CATEGORY_WOWPROGRESSLINK = "WoWProgressLink"
-BINDING_NAME_WOWPROGRESSLINK = "Link by Mouseover"
+BINDING_NAME_WOWPROGRESSLINK = "WoWProgress link (mouseover)"
+BINDING_NAME_RAIDERIOLINK = "Raider.IO link (mouseover)"
 
 local function getRegion()
     local regionLabel = {"us", "kr", "eu", "tw", "cn"}
@@ -9,7 +10,7 @@ end
 
 local realmTranslation = WoWProgressLink.translation
 
-local function buildLink(name)
+local function buildLink(name, isAlternativeLink)
     local char, server = string.match(name, "(.-)-(.*)")
     if not char then
         char = name
@@ -25,11 +26,19 @@ local function buildLink(name)
     end
     
     local region = getRegion()
-    return "https://www.wowprogress.com/character/" .. region .. "/" .. server .. "/" .. char
+    local prefix
+    
+    if isAlternativeLink then
+        prefix = "https://raider.io/characters/"
+    else
+        prefix = "https://www.wowprogress.com/character/"
+    end
+    
+    return prefix .. region .. "/" .. server .. "/" .. char
 end
 
-local function pasteLink(name)
-    local link = buildLink(name)
+local function pasteLink(name, isAlternativeLink)
+    local link = buildLink(name, isAlternativeLink)
 
     local editBox = ChatEdit_ChooseBoxForSend()
     ChatEdit_ActivateChat(editBox)
@@ -40,8 +49,8 @@ end
 local function applicantLink(self, button, down)
     if button == "LeftButton" then
         local applicant = C_LFGList.GetApplicantMemberInfo(self:GetParent().applicantID, self.memberIdx)
-              
-        pasteLink(applicant)
+
+        pasteLink(applicant, IsModifiedClick("SELFCAST"))
     end
 end
 
@@ -49,8 +58,8 @@ local function leaderLink(self, button, down)
     if button == "LeftButton" then
         local results = {C_LFGList.GetSearchResultInfo(self.resultID)}
         local leader = results[13]
-              
-        pasteLink(leader)
+
+        pasteLink(leader, IsModifiedClick("SELFCAST"))
     end
 end
 
@@ -62,6 +71,20 @@ function WoWProgressLink()
             pasteLink(name .. "-" .. realm)
         else
             pasteLink(name)
+        end    
+    else
+        print("WoWProgressLink: Unit is not a player or not in range.")
+    end     
+end
+
+function RaiderIOLink()
+    if UnitIsPlayer("mouseover") then
+        local name, realm = UnitName("mouseover")
+        
+        if realm then
+            pasteLink(name .. "-" .. realm, true)
+        else
+            pasteLink(name, true)
         end    
     else
         print("WoWProgressLink: Unit is not a player or not in range.")
